9146dd3d3c9783b3fbd0e3ec17f32df6181eb211
Samundra Dhakal <samundra.dhakal@tuni.fi>
2022-09-06 16:08:21 +0300

    initial setup for web form

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SignUpForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SignUpForm from "./components/Sign_Up.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignUpForm
   }
 }
 </script>
